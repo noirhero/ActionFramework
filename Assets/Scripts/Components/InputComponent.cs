5e2bc0e1f974020ef0f2569455ef3dbb53daa12d
hjkim <khj00767@naver.com>
2020-05-16 12:49:31 +0900

    이동을 위한 Input 작업중

---
+++ Assets/Scripts/Components/InputComponent.cs
@@ -0,0 +1,26 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using System;
+using Unity.Entities;
+using UnityEngine;
+
+[Serializable]
+public struct InputDataComponent : IComponentData {
+    public int state;
+}
+
+[Serializable]
+public struct InputMoveComponent : IComponentData {
+    public Vector2 value;
+    public float accumTime;
+}
+
+[Serializable]
+public struct InputJumpComponent : IComponentData {
+    public float accumTime;
+}
+
+[Serializable]
+public struct InputAttackComponent : IComponentData {
+    public float accumTime;
+}
