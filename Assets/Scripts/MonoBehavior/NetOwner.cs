c57f78323bd31562c984e5c7fb4a1de1f82ac350
nhlee <noirhero83@gmail.com>
2020-06-27 11:42:56 +0900

    네트워크 시스템 기반 구축.

---
+++ Assets/Scripts/MonoBehavior/NetOwner.cs
@@ -0,0 +1,10 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using Unity.Entities;
+using UnityEngine;
+
+public class NetOwner : MonoBehaviour {
+    void Start() {
+        World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<NetOwnerSystem>().Enabled = true;
+    }
+}
---
+++ Assets/Scripts/MonoBehavior/ServerOwner.cs
@@ -0,0 +1,10 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using Unity.Entities;
+using UnityEngine;
+
+public class ServerOwner : MonoBehaviour {
+    void Start() {
+        World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<ServerSystem>().Enabled = true;
+    }
+}
