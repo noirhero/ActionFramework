8c950040816c3268e78a7fac2dcb19660a055032
nhlee <noirhero83@gmail.com>
2020-07-15 00:00:15 +0900

    카메라 따라가기 시스템 추가.

---
+++ Assets/Scripts/Systems/CameraFollowSystem.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CameraFollowSystem : SystemBase {
+    private Transform _cameraTransform;
+    protected override void OnStartRunning() {
+        if (false == ReferenceEquals(null, Camera.main)) {
+            _cameraTransform = Camera.main.transform;
+        }
+    }
+
+    protected override void OnUpdate() {
+        var camPos = _cameraTransform.position;
+        var deltaTime = Time.DeltaTime;
+        Entities
+            .WithoutBurst()
+            .WithAll<CameraFollowComponent>()
+            .ForEach((in Translation pos) => {
+                camPos.x = math.lerp(pos.Value.x, camPos.x, deltaTime);
+            })
+            .Run();
+        _cameraTransform.position = camPos;
+    }
+}
