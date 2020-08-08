f928cf974a3d9cbee0c3bfd2b7ad94891c0e7637
nhlee <noirhero83@gmail.com>
2020-08-08 12:17:44 +0900

    맵 오브젝트 추가.

---
+++ Assets/Resources/Backgrounds/CountryRoad/Objects.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Objects" tilewidth="73" tileheight="68" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="39" height="35" source="crate.png"/>
+ </tile>
+ <tile id="1">
+  <image width="73" height="68" source="crate-stack.png"/>
+ </tile>
+ <tile id="2">
+  <image width="24" height="30" source="barrel.png"/>
+ </tile>
+</tileset>
