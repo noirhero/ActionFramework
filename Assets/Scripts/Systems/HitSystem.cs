f4ac5460816649a16cf7f610fb4b6b66634b0d1b
nhlee <noirhero83@gmail.com>
2020-08-08 12:35:47 +0900

    프레임 스킵 시스템 시간 조정.

--- Assets/Scripts/Systems/HitSystem.cs
+++ Assets/Scripts/Systems/HitSystem.cs
@@ -13,7 +13,7 @@ public class HitSystem : SystemBase {
                 if (false == AnimUtility.HasState(animComp, AnimUtility.hit)) {
                     animComp.state |= AnimUtility.hit;
                     renderer.color = hitComp.hitEffectColor;
-                    GamePause.Pause(0.08f);
+                    GamePause.Pause(0.15f);
                 }
 
                 hitComp.elapsedTime += Time.DeltaTime;
