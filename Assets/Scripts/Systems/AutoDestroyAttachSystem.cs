f2ca7d0cc49f7a85dd21daf61d0bf322223d502f
nhlee <noirhero83@gmail.com>
2020-08-20 00:33:23 +0900

    AutoDestroySystem 분리.

---
+++ Assets/Scripts/Systems/AutoDestroyAttachSystem.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using Unity.Entities;
+
+public class AutoDestroyAttachSystem : SystemBase {
+    protected override void OnUpdate() {
+        Entities
+            .WithName("AutoDestroyAttachSystem")
+            .WithoutBurst()
+            .WithStructuralChanges()
+            .WithNone<FindSpritePresetComponent>()
+            .ForEach((Entity entity, in SpritePresetComponent preset, in FindAutoDestroyComponent findDestroyComp) => {
+                if (0 == findDestroyComp.loopingCount) {
+                    EntityManager.RemoveComponent<FindAutoDestroyComponent>(entity);
+                    return;
+                }
+
+                if (false == preset.value.datas.TryGetValue(AnimUtility.AnimKey.Run, out var animData)) {
+                    return;
+                }
+
+                EntityManager.AddComponentData(entity, new AutoDestroyComponent {
+                    lifeTime = animData.length * findDestroyComp.loopingCount
+                });
+                EntityManager.RemoveComponent<FindAutoDestroyComponent>(entity);
+            })
+            .Run();
+    }
+}
--- Assets/Scripts/Systems/AutoDestroySystem.cs
+++ Assets/Scripts/Systems/AutoDestroySystem.cs
@@ -2,11 +2,9 @@
 
 using Unity.Burst;
 using Unity.Entities;
-using System.Collections.Generic;
 
 public class AutoDestroySystem : SystemBase {
     private EntityCommandBufferSystem _cmdBufSystem;
-
     protected override void OnCreate() {
         _cmdBufSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
@@ -14,27 +12,6 @@ public class AutoDestroySystem : SystemBase {
     protected override void OnUpdate() {
         var deltaTime = Time.DeltaTime;
         var cmdBuf = _cmdBufSystem.CreateCommandBuffer().AsParallelWriter();
-
-        Entities.WithName("AutoDestroySystem_Find")
-            .WithBurst(FloatMode.Default, FloatPrecision.Standard, true)
-            .WithStructuralChanges()
-            .ForEach(
-                (Entity entity, SpritePresetComponent preset, ref FindAutoDestroyComponent findDestroyComp) => {
-                    if (0 == findDestroyComp.loopingCount) {
-                        return;
-                    }
-
-                    if (false == preset.value.datas.TryGetValue(AnimUtility.AnimKey.Run, out var animData)) {
-                        return;
-                    }
-
-                    EntityManager.RemoveComponent<FindAutoDestroyComponent>(entity);
-                    EntityManager.AddComponentData(entity, new AutoDestroyComponent {
-                        lifeTime = animData.length * findDestroyComp.loopingCount
-                    });
-                })
-            .Run();
-
         Entities.WithName("AutoDestroySystem")
             .WithBurst(FloatMode.Default, FloatPrecision.Standard, true)
             .ForEach((Entity entity, int entityInQueryIndex, ref AutoDestroyComponent autoDestroy) => {
