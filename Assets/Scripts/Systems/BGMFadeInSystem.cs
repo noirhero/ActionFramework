bcc9dfa561fd9400742f83628be6bbdc2ee2a441
nhlee <noirhero83@gmail.com>
2020-08-22 12:34:55 +0900

    BGM 페이드 인, 아웃 시스템 추가.

---
+++ Assets/Scripts/Systems/BGMFadeInSystem.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BGMFadeInSystem : SystemBase {
+    protected override void OnUpdate() {
+        var audioSources = new Dictionary<SoundUtility.SourceKey, AudioSource>();
+        Entities
+            .WithName("BGMFadeInSystem_Collecting")
+            .WithoutBurst()
+            .ForEach((in AudioSourceComponent audioSource) => {
+                audioSources.Add(audioSource.id, audioSource.source);
+            })
+            .Run();
+
+        var deltaTime = Time.DeltaTime;
+        Entities
+            .WithName("BGMFadeInSystem")
+            .WithoutBurst()
+            .WithStructuralChanges()
+            .ForEach((Entity entity, in AudioSourceControlComponent audioSourceControl) => {
+                if (false == audioSources.TryGetValue(audioSourceControl.id, out var audioSource)) {
+                    return;
+                }
+
+                var delta = audioSourceControl.accume * deltaTime;
+                audioSource.volume = math.min(audioSourceControl.maxVolume, audioSource.volume + delta);
+                if (audioSourceControl.maxVolume <= audioSource.volume) {
+                    EntityManager.DestroyEntity(entity);
+                }
+            })
+            .Run();
+    }
+}
---
+++ Assets/Scripts/Systems/BGMFadeOutSystem.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BGMFadeOutSystem : SystemBase {
+    protected override void OnUpdate() {
+        var audioSources = new Dictionary<SoundUtility.SourceKey, AudioSource>();
+        Entities
+            .WithName("BGMFadeOutSystem_Collecting")
+            .WithoutBurst()
+            .ForEach((in AudioSourceComponent audioSource) => {
+                audioSources.Add(audioSource.id, audioSource.source);
+            })
+            .Run();
+
+        var deltaTime = Time.DeltaTime;
+        Entities
+            .WithName("BGMFadeOutSystem")
+            .WithoutBurst()
+            .WithStructuralChanges()
+            .ForEach((Entity entity, in AudioSourceControlComponent audioSourceControl) => {
+                if (false == audioSources.TryGetValue(audioSourceControl.id, out var audioSource)) {
+                    return;
+                }
+
+                var delta = audioSourceControl.accume * deltaTime;
+                audioSource.volume = math.max(audioSourceControl.minVolume, audioSource.volume - delta);
+                if (audioSourceControl.minVolume >= audioSource.volume) {
+                    EntityManager.DestroyEntity(entity);
+                }
+            })
+            .Run();
+    }
+}
