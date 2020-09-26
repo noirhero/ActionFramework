2befad279488fe7d4b58351e618e360fca0be212
nhlee <noirhero83@gmail.com>
2020-09-26 11:01:34 +0900

    배경 변화

---
+++ Assets/Resources/Backgrounds/Temple/Backgrounds.tsx
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Backgrounds" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
---
+++ Assets/Resources/Backgrounds/Temple/Images.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Images" tilewidth="384" tileheight="224" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="21" source="back-bush-1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="17" source="back-bush-2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="112" height="112" source="back-trees.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="112" source="back-trees-tile.png"/>
+ </tile>
+ <tile id="4">
+  <image width="384" height="224" source="bg-sky-moon.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="80" source="bg-trees.png"/>
+ </tile>
+ <tile id="6">
+  <image width="384" height="80" source="bg-trees-row.png"/>
+ </tile>
+ <tile id="7">
+  <image width="45" height="47" source="big-rock.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="34" source="bush.png"/>
+ </tile>
+ <tile id="9">
+  <image width="384" height="112" source="front-clouds.png"/>
+ </tile>
+ <tile id="10">
+  <image width="95" height="95" source="moon.png"/>
+ </tile>
+ <tile id="11">
+  <image width="384" height="176" source="mountains.png"/>
+ </tile>
+ <tile id="12">
+  <image width="192" height="224" source="sky.png"/>
+ </tile>
+ <tile id="13">
+  <image width="34" height="18" source="small-rock.png"/>
+ </tile>
+ <tile id="15">
+  <image width="65" height="161" source="tree.png"/>
+ </tile>
+</tileset>
