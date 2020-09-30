8e3ec980a72af5cfd3e2e0760ef7503bf3ad8d75
noirhero <noirhero83@gmail.com>
2020-09-30 22:36:25 +0900

    배경 프랍 정리.

--- Assets/Resources/Backgrounds/Temple/Images.tsx
+++ Assets/Resources/Backgrounds/Temple/Images.tsx
@@ -1,32 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Images" tilewidth="384" tileheight="224" tilecount="15" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="Images" tilewidth="384" tileheight="224" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="16" height="21" source="back-bush-1.png"/>
- </tile>
- <tile id="1">
-  <image width="24" height="17" source="back-bush-2.png"/>
- </tile>
  <tile id="2">
   <image width="112" height="112" source="back-trees.png"/>
  </tile>
  <tile id="3">
   <image width="64" height="112" source="back-trees-tile.png"/>
  </tile>
- <tile id="4">
-  <image width="384" height="224" source="bg-sky-moon.png"/>
- </tile>
- <tile id="5">
-  <image width="64" height="80" source="bg-trees.png"/>
- </tile>
  <tile id="6">
   <image width="384" height="80" source="bg-trees-row.png"/>
- </tile>
- <tile id="7">
-  <image width="45" height="47" source="big-rock.png"/>
- </tile>
- <tile id="8">
-  <image width="32" height="34" source="bush.png"/>
  </tile>
  <tile id="9">
   <image width="384" height="112" source="front-clouds.png"/>
@@ -41,9 +23,30 @@
   <image width="192" height="224" source="sky.png"/>
  </tile>
  <tile id="13">
-  <image width="34" height="18" source="small-rock.png"/>
+  <image width="16" height="21" source="back-bush-1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="24" height="17" source="back-bush-2.png"/>
  </tile>
  <tile id="15">
+  <image width="24" height="30" source="barrel.png"/>
+ </tile>
+ <tile id="16">
+  <image width="45" height="47" source="big-rock.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="34" source="bush.png"/>
+ </tile>
+ <tile id="18">
+  <image width="39" height="35" source="crate.png"/>
+ </tile>
+ <tile id="19">
+  <image width="73" height="68" source="crate-stack.png"/>
+ </tile>
+ <tile id="20">
+  <image width="34" height="18" source="small-rock.png"/>
+ </tile>
+ <tile id="21">
   <image width="65" height="161" source="tree.png"/>
  </tile>
 </tileset>
--- Assets/Resources/Backgrounds/Temple/Tileset.tsx
+++ Assets/Resources/Backgrounds/Temple/Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Tileset" tilewidth="16" tileheight="16" tilecount="169" columns="13">
- <image source="../Temple/tileset.png" width="208" height="208"/>
+ <image source="tileset.png" width="208" height="208"/>
 </tileset>
